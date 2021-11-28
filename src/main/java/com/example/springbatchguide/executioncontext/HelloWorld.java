72724cb7eddafe8517ce49d7f4332943e6def3ae
hooneats <hooneats@naver.com>
2021-11-28 17:53:49 +0900

    03-잡리스너사용하기-ExecutionContext 에 name 데이터 추가하기(2) 스텝

--- src/main/java/com/example/springbatchguide/executioncontext/HelloWorld.java
+++ src/main/java/com/example/springbatchguide/executioncontext/HelloWorld.java
@@ -8,15 +8,23 @@ import org.springframework.batch.repeat.RepeatStatus;
 
 public class HelloWorld implements Tasklet {
     private static final String HELLO_WORLD = "Hello, %s";
+
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+        //
         String name = (String) chunkContext.getStepContext()
                 .getJobParameters()
                 .get("name");
 
+        // 잡의 ExecutionContext 를 가져오는 방법
+//        ExecutionContext jobContext = chunkContext.getStepContext()
+//                .getStepExecution()
+//                .getJobExecution()
+//                .getExecutionContext();
+
+        // 스텝의 ExecutionContext 를 가져와 조작하는 방식도  .getJobExecution() 를 제회하면된다.
         ExecutionContext jobContext = chunkContext.getStepContext()
                 .getStepExecution()
-                .getJobExecution()
                 .getExecutionContext();
 
         jobContext.put("user.name", name);
