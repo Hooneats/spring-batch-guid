3ee14c4f05ebcfd85dfb396fd6d093ff3b09ab6e
hooneats <hooneats@naver.com>
2021-11-28 15:46:04 +0900

    01-잡구성하기-늦은바인딩을 활용해 잡파라미터 접근

--- src/main/java/com/example/springbatchguide/SpringBatchGuideApplication.java
+++ src/main/java/com/example/springbatchguide/SpringBatchGuideApplication.java
@@ -6,10 +6,12 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -29,14 +31,26 @@ public class SpringBatchGuideApplication {
     public Step step1() {
         //
         return this.stepBuilderFactory.get("step1")
-                .tasklet(helloWorldTasklet()).build();
+                .tasklet(TaskletHelloWorldTasklet(null)).build();
     }
 
-    private Tasklet helloWorldTasklet() {
+    @Bean
+    public Tasklet helloWorldTasklet() {
         return (stepContribution, chunkContext) -> {
             String name = (String) chunkContext.getStepContext()
                     .getJobParameters()
                     .get("name");
+            System.out.println(String.format("Hello, %s!", name));
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet TaskletHelloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name
+    ) {
+        return (stepContribution, chunkContext) -> {
             System.out.println(String.format("Hello, %s!", name));
             return RepeatStatus.FINISHED;
         };
